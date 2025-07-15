ee20daf4b8ded93f8983c388f4128d084d020ab8
Yash Dive <yashdive190@gmail.com>
2025-07-15 22:22:48 +0530

    Created data ingestion configuration and pipeline

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import sys
+import os
+sys.path.append(os.path.abspath("src"))
+
 from henalyze_CNN_classifier import logger
+from henalyze_CNN_classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to my custom logger!")
 
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
---
+++ src/henalyze_CNN_classifier/constants/__init__.py
@@ -0,0 +1,6 @@
+#We'll be reading this only once
+
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
