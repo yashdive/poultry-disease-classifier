98865794e1f4de6e7d1136ce68726451b872eb7d
Yash Dive <yashdive190@gmail.com>
2025-07-22 19:01:18 +0530

    Training pipeline created

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import sys
 import os
+
 sys.path.append(os.path.abspath("src"))
 
 from henalyze_CNN_classifier import logger
 from henalyze_CNN_classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from henalyze_CNN_classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline  
+from henalyze_CNN_classifier.pipeline.stage_03_training import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -27,3 +29,15 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+     
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
---
+++ src/henalyze_CNN_classifier/pipeline/stage_02_prepare_base_model.py
@@ -0,0 +1,32 @@
+from henalyze_CNN_classifier import logger
+from henalyze_CNN_classifier.components.prepare_base_model import PrepareBaseModel
+from henalyze_CNN_classifier.config.configuration import ConfigurationManager
+
+
+STAGE_NAME = "Prepare base model"
+
+class PrepareBaseModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        prepare_base_model_config = config.get_prepare_base_model_config()
+        prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
+        prepare_base_model.get_base_model()
+        prepare_base_model.update_base_model()
+
+
+
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
